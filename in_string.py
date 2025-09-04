b624ca9bba8695b8671b656716063cd5c747d0d9
LernerJoaquin <lernerjoaquin@gmail.com>
2025-09-04 16:42:55 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,7 +1,7 @@
 def check_vowels():
     name = input("Ingresá tu nombre: ")
-    print(f"Contiene a: {"a" in name.lower()}")
-    print(f"Contiene e: {"e" in name.lower()}")
-    print(f"Contiene i: {"i" in name.lower()}")
-    print(f"Contiene o: {"o" in name.lower()}")
-    print(f"Contiene u: {"u" in name.lower()}")
+    print(f"Contiene a: {str("a" in name.lower())}")
+    print(f"Contiene e: {str("e" in name.lower())}")
+    print(f"Contiene i: {str("i" in name.lower())}")
+    print(f"Contiene o: {str("o" in name.lower())}")
+    print(f"Contiene u: {str("u" in name.lower())}")
