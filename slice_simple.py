7991af4a0739c941922f6b4762906aa5656038b2
LernerJoaquin <lernerjoaquin@gmail.com>
2025-11-27 13:47:33 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,6 +1,5 @@
 def slice_simple():
-    texto= "Awesome"
-    texto1 = texto.lower()
-    print(texto1[0:3])
-    print(texto1[2:5])
-    print(texto1)
+    texto = "Awesome"
+    print(texto[0:3].lower())
+    print(texto[2:5])
+    print(texto.lower())
